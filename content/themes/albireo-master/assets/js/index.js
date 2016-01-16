472c4d8854439e2c91a539dbbfe0daf2f2382da7
António Regadas <antonio.regadas@premium-minds.com>
2016-01-16 11:47:50 +0000

    menu changes

--- content/themes/albireo-master/assets/js/index.js
+++ content/themes/albireo-master/assets/js/index.js
@@ -7,7 +7,24 @@
 
     $(document).ready(function(){
 
-        /* Empty */
+    var mainMenu = function () {
+        var menuOvrl = $('.js-main-menu-ovrl'),
+            menuHamburger = $('.js-main-menu-open');
+        $('.js-main-menu-open').on('click', function (e) {
+            e.preventDefault();
+            menuHamburger.hide();
+            menuOvrl.addClass('open');
+        });
+        $('.js-main-menu-close').on('click', function (e) {
+            e.preventDefault();
+            menuOvrl.removeClass('open');
+            menuHamburger.show();
+        });
+    }
+
+    
+mainMenu();
+
 
     });
 
