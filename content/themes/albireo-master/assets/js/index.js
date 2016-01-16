7a0c62bc042c2352961321ea4eaabf5fe4fb0b2e
António Regadas <antonio.regadas@premium-minds.com>
2016-01-16 12:19:28 +0000

    menu changes font-awesome

--- content/themes/albireo-master/assets/js/index.js
+++ content/themes/albireo-master/assets/js/index.js
@@ -7,24 +7,21 @@
 
     $(document).ready(function(){
 
-    var mainMenu = function () {
-        var menuOvrl = $('.js-main-menu-ovrl'),
-            menuHamburger = $('.js-main-menu-open');
-        $('.js-main-menu-open').on('click', function (e) {
-            e.preventDefault();
-            menuHamburger.hide();
-            menuOvrl.addClass('open');
-        });
-        $('.js-main-menu-close').on('click', function (e) {
-            e.preventDefault();
-            menuOvrl.removeClass('open');
-            menuHamburger.show();
-        });
-    }
-
-    
-mainMenu();
-
+	    var mainMenu = function () {
+	        var menuOvrl = $('.js-main-menu-ovrl'),
+	            menuHamburger = $('.js-main-menu-open');
+	        $('.js-main-menu-open').on('click', function (e) {
+	            e.preventDefault();
+	            menuHamburger.hide();
+	            menuOvrl.addClass('open');
+	        });
+	        $('.js-main-menu-close').on('click', function (e) {
+	            e.preventDefault();
+	            menuOvrl.removeClass('open');
+	            menuHamburger.show();
+	        });
+	    }
+		mainMenu();
 
     });
 
